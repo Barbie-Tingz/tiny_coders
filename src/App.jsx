77adfbbde7b71cfd4e4d33413434f81ff6c17a07
Barbie-Tingz <jillianhenry0823@gmail.com>
2026-07-29 20:53:27 -0400

    First edits

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function AboutPage() {
+    return(
+        <>
+        <h1>About</h1>
+        <p>## About Text Goes Here ##</p>
+        </>
+    )
+}
+
+function App() {
+    return <AboutPage/>
+}
+
+export default App 
